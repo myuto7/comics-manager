4b74fb5a337c40f1991d423b35d1c5496f4b2c13
myuto <m.yuto72319@gmail.com>
2025-04-27 04:57:54 +0900

    feat: マンガの登録と表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+
 import { Box, Typography } from "@mui/material";
+import ComicTable from "./components/ComicTable";
+import { RegisterModal } from "./components/RegisterModal";
+import { useState, useEffect } from "react";
+import { Comic } from "./type";
 
 export default function Home() {
+  const [comics, setComics] = useState<Comic[]>([]);
+
+  const fetchComics = async () => {
+    const res = await fetch("/api/notion");
+    const data = await res.json();
+    setComics(data);
+  };
+
+  useEffect(() => {
+    fetchComics();
+  }, []);
+
   return (
-    <Box>
-      <Typography variant="h1" gutterBottom fontSize={24}>
+    <Box width="100%" py={4}>
+      <Typography
+        variant="h1"
+        gutterBottom
+        fontSize={24}
+        textAlign="center"
+        mb={4}
+      >
         マンガ購入管理アプリ
       </Typography>
+      <RegisterModal onRegisterSuccess={fetchComics} />
+      <ComicTable comics={comics} />
     </Box>
   );
 }
---
+++ src/app/type.ts
@@ -0,0 +1,11 @@
+export type Comic = {
+  id: string;
+  title: string;
+  creator: string;
+  isPurchased: boolean;
+};
+
+export type FormValues = {
+  title: string;
+  creator: string;
+};
