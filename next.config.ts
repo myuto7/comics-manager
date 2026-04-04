3f32883d3632ab7cfd1a1531bd0b2e25156c756e
myuto <m.yuto72319@gmail.com>
2026-04-04 14:54:49 +0900

    fix: nextConfigのimageの設定を修正

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["uploads.mangadex.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploads.mangadex.org",
+        pathname: "/covers/**",
+      },
+    ],
   },
 };
 
